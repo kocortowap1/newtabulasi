fd0ad7ef703ff0711529c291bc0945b247b52603
kocortowap1 <kocortowap@gmail.com>
2018-04-07 11:29:22 +0700

    revisi controller tes

--- application/controllers/Tes.php
+++ application/controllers/Tes.php
@@ -12,14 +12,7 @@ class Tes extends CI_Controller {
         $this->load->library('excel');
         $data = $this->excel->read_excel($path);
 
-        /* $filetype = "Xls";
-        $inputFileType = \PhpOffice\PhpSpreadsheet\IOFactory::identify($path);
-        $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader($inputFileType);
-        $xls = $reader->load($path);
-        $sheet = $xls->getActiveSheet();
-        $last_row = $sheet->getHighestRow();
-        $last_col = $sheet->getHighestColumn();
-        $data = $sheet->rangeToArray('A1:'.$last_col.$last_row,NULL, TRUE,TRUE,TRUE); */
+
         echo "<pre>";
         print_r($data);
         echo "</pre>";
